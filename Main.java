e1b63b48f8fa936a1d3ca9acc9b25f84654b920e
Lenic3 <lenicems06@gmail.com>
2024-08-21 22:12:15 +0000

    I finished lab 1

--- Main.java
+++ Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String name = "Robby";
+        String birthday = "08/21/2024";
+
+        System.out.println("Welcome to CS 112!");
+
+        
+
+        askHowDoing(name);
+        System.out.println(userBirthday(birthday));
+
+
     }
+
+    public static String userBirthday(String birthday) {
+        String personBirthday = "Robby's birthdate is "+ birthday;
+        return personBirthday;
+    }
+    
+    public static void askHowDoing(String name) {
+        System.out.println("How are you doing? " + name);
+    } 
 }
